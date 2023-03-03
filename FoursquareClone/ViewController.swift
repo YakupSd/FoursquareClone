e88e85ea33b89b81ea70b0985535e96bb1535827
yakupsuda <yakupsuda@gmail.com>
2023-03-03 17:26:50 +0300

    Initial Commit

---
+++ FoursquareClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by Yakup Suda on 3.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
